723f2a31458427f3fbe68228684494ad386e143d
Jesús Moreno <j.moreno@abas-bs.com>
2023-06-14 12:35:09 +0200

    Adverts from state

--- src/components/adverts/AdvertPage.js
+++ src/components/adverts/AdvertPage.js
@@ -1,27 +1,30 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Layout from '../layout/Layout';
-import { deleteAdvert, getAdvert } from './service';
+import { deleteAdvert } from './service';
 import './Advert.css';
+import { useSelector } from 'react-redux';
+import { getAdvert } from '../../store/selectors';
 
 function AdvertsPage() {
   const params = useParams();
   const navigate = useNavigate();
-  const [advert, setAdvert] = useState([]);
+  // const [advert, setAdvert] = useState([]);
+  const advert = useSelector(getAdvert(params.advertId));
 
-  useEffect(() => {
-    async function getData() {
-      try {
-        const advertDetail = await getAdvert(params.advertId);
-        setAdvert(advertDetail);
-      } catch (error) {
-        if (error.status === 404) {
-          return navigate('/404');
-        }
-      }
-    }
-    getData();
-  }, [navigate, params]);
+  // useEffect(() => {
+  //   async function getData() {
+  //     try {
+  //       const advertDetail = await getAdvert(params.advertId);
+  //       setAdvert(advertDetail);
+  //     } catch (error) {
+  //       if (error.status === 404) {
+  //         return navigate('/404');
+  //       }
+  //     }
+  //   }
+  //   getData();
+  // }, [navigate, params]);
 
   const handleDelete = async () => {
     if (window.confirm('¿Está seguro de eliminar el anuncio?')) {
@@ -34,19 +37,20 @@ function AdvertsPage() {
     }
   };
 
-  let color = advert.sale ? 'color-green' : 'color-blue';
+  let color = advert?.sale ? 'color-green' : 'color-blue';
   return (
     <Layout>
       <h1>Detalle de anuncio</h1>
-      <img className='img-detail' src={advert.photo} alt='Imagen del anuncio' />
-      <p className='title'>{advert.name}</p>
-      <p className='price'>{advert.price}€</p>
-      <p className={color}>{advert.sale ? 'Se vende' : 'Se compra'}</p>
-      <div>
-        {advert.tags &&
-          advert.tags.map((tag) => <span className='tag'> {tag} </span>)}
-      </div>
-      <button onClick={handleDelete}> Pulse para eliminar el anuncio</button>
+      {advert && (
+        <>
+          <img className='img-detail' src={advert.photo} alt='Imagen del anuncio' />
+          <p className='title'>{advert.name}</p>
+          <p className='price'>{advert.price}€</p>
+          <p className={color}>{advert.sale ? 'Se vende' : 'Se compra'}</p>
+          <div>{advert.tags && advert.tags.map((tag) => <span className='tag'> {tag} </span>)}</div>
+          <button onClick={handleDelete}> Pulse para eliminar el anuncio</button>
+        </>
+      )}
     </Layout>
   );
 }
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,2 +1,7 @@
 export const getIsLogged = (state) => state.auth;
 export const getAdvertisements = (state) => state.advertisements;
+// export const getAdvert = (state, advertId) => getAdvertisements(state).find((advert) => advert.id === +advertId);
+
+export const getAdvert = (advertId) => (state) => {
+  return getAdvertisements(state).find((advert) => advert.id === advertId);
+};
